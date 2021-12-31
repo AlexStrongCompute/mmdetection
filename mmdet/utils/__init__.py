a230986083afa155d0a2eed382b30f8279cb5ff6
Omer Hodzic <ohod6149@uni.sydney.edu.au>
2021-12-31 13:35:34 +1100

    merge conflict resolved

--- mmdet/utils/__init__.py
+++ mmdet/utils/__init__.py
@@ -3,9 +3,5 @@ from .collect_env import collect_env
 from .logger import get_root_logger
 from .rbbox_visualization import imshow_det_rbboxes
 
-<<<<<<< HEAD
-__all__ = ['get_root_logger', 'collect_env']
-=======
 __all__ = ['get_model_complexity_info', 'get_root_logger', 'collect_env',
            'imshow_det_rbboxes']
->>>>>>> fork/master
