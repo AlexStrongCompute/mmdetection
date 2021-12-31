b85307aac22baeeacf246a03ce8f5741a94b756b
Omer Hodzic <ohod6149@uni.sydney.edu.au>
2021-12-31 13:30:46 +1100

    merge conflict resolved

--- mmdet/models/dense_heads/__init__.py
+++ mmdet/models/dense_heads/__init__.py
@@ -31,12 +31,13 @@ from .rpn_head import RPNHead
 from .sabl_retina_head import SABLRetinaHead
 from .solo_head import DecoupledSOLOHead, DecoupledSOLOLightHead, SOLOHead
 from .ssd_head import SSDHead
-<<<<<<< HEAD
 from .vfnet_head import VFNetHead
 from .yolact_head import YOLACTHead, YOLACTProtonet, YOLACTSegmHead
 from .yolo_head import YOLOV3Head
 from .yolof_head import YOLOFHead
 from .yolox_head import YOLOXHead
+from .rretina_head import RRetinaHead
+from .rretina_refine_head import RRetinaRefineHead
 
 __all__ = [
     'AnchorFreeHead', 'AnchorHead', 'GuidedAnchorHead', 'FeatureAdaption',
@@ -49,16 +50,5 @@ __all__ = [
     'CascadeRPNHead', 'EmbeddingRPNHead', 'LDHead', 'CascadeRPNHead',
     'AutoAssignHead', 'DETRHead', 'YOLOFHead', 'DeformableDETRHead',
     'SOLOHead', 'DecoupledSOLOHead', 'CenterNetHead', 'YOLOXHead',
-    'DecoupledSOLOLightHead', 'LADHead'
-=======
-from .rretina_head import RRetinaHead
-from .rretina_refine_head import RRetinaRefineHead
-
-__all__ = [
-    'AnchorHead', 'GuidedAnchorHead', 'FeatureAdaption', 'RPNHead',
-    'GARPNHead', 'RetinaHead', 'RetinaSepBNHead', 'GARetinaHead', 'SSDHead',
-    'FCOSHead', 'RepPointsHead', 'FoveaHead', 'FreeAnchorRetinaHead',
-    'ATSSHead', 'FSAFHead', 'NASFCOSHead', 'PISARetinaHead', 'PISASSDHead',
-    'RRetinaHead', 'RRetinaRefineHead'
->>>>>>> fork/master
+    'DecoupledSOLOLightHead', 'LADHead', 'RRetinaHead', 'RRetinaRefineHead' 
 ]
